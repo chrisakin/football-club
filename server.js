1425476cb49d3c73298c99c6cfcfc91f8cb0cc3a
Christopher Akintade <chrisfemide@gmail.com>
2020-08-08 18:46:17 +0100

    Final

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-/** 
-  //Install express server
+
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/football-clubs'));
+app.use(express.static(__dirname + '/dist/football-clubs'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/football-clubs/index.html'));
+res.sendFile(path.join(__dirname+'/dist/football-clubs/index.html'));
 });
-//console.log(8080)
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-console.log('app listening at port 8080')*/
-
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static('./dist/football-clubs'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/football-clubs/'}
-);
-});
-
-app.listen(process.env.PORT || 8080);
-console.log('app running at port 8080')
--- src/assets/js/plugins/classie.js
+++ src/assets/js/plugins/classie.js
@@ -25,30 +25,7 @@ function classReg( className ) {
 // altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
-    }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
+
 
 function toggleClass( elem, c ) {
   var fn = hasClass( elem, c ) ? removeClass : addClass;
