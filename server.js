7a9e0078b35c04b9f0b20448c4a2c3fcb6895bfd
Christopher Akintade <chrisfemide@gmail.com>
2020-08-08 17:15:07 +0100

    Final

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-
-
+/** 
+  //Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/football-club'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/football-clubs'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/football-club' }
-  );
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/football-clubs/index.html'));
+});
+//console.log(8080)
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+console.log('app listening at port 8080')*/
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/football-clubs'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/football-clubs/'}
+);
 });
 
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
+console.log('app running at port 8080')
